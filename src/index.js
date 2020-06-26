9283e2825a2887f9a9fd07790b51d7a54cce8705
codingbody <shawncoding21@gmail.com>
2020-06-26 20:35:09 +0900

    integration test for three js and panolens

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import * as THREE from 'three';
+import { Scene } from 'three';
+
+const scene = new Scene();
+const scene = new THREE.Scene();
+
+console.log("hi")
+
+console.log(scene, "scene")
+console.log(scene)
